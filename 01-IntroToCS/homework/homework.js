642a49a656a7f4b0d940e1282a9512b0326b051b
MatiasSoto21 <matiassoto00@outlook.com>
2022-07-04 17:41:03 -0300

    homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+    var array = num.split('');
+    let sum = 0;
 
+    for (let i = 0; i < array.length; i++) {
+       sum = sum +num[i] * Math.pow(2, array.length - 1 - i);
+    }
+    return sum;
+  
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+var array = [];
+while (num > 0){
+  array.unshift(num%2);
+  num = Math.floor(num/2);
+}
+return array.join('');
 }
 
 
